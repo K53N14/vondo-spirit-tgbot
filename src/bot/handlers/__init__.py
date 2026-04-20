474e47e27259269ff90a442f1f7caeb4c314be15
K53N14 <71764579+K53N14@users.noreply.github.com>
2026-04-20 12:41:08 +0700

    Add admin promotion, rank, and rights management commands

---
+++ src/bot/handlers/__init__.py
@@ -0,0 +1,40 @@
+from bot.handlers.admin import (
+    promote_admin_command,
+    remove_everywhere,
+    set_admin_rank_command,
+    set_admin_rights_command,
+)
+from bot.handlers.chat_member import on_chat_member_update, on_my_chat_member_update
+from bot.handlers.common import (
+    add_users_command,
+    delete_user_command,
+    groups_command,
+    help_command,
+    list_users_command,
+    sync_everyone_command,
+    refresh_groups_command,
+    remove_group_command,
+    start_command,
+    sync_me_command,
+    user_groups_command,
+)
+
+__all__ = [
+    "on_chat_member_update",
+    "on_my_chat_member_update",
+    "remove_everywhere",
+    "promote_admin_command",
+    "set_admin_rank_command",
+    "set_admin_rights_command",
+    "start_command",
+    "help_command",
+    "add_users_command",
+    "delete_user_command",
+    "sync_me_command",
+    "groups_command",
+    "remove_group_command",
+    "refresh_groups_command",
+    "list_users_command",
+    "sync_everyone_command",
+    "user_groups_command",
+]
